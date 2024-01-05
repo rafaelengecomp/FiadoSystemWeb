d8516b6d6f28bd2ae1bfca7c3aafb65a588b98d3
Rafael Andrade de Moura <rafael.moura@programmers.com.br>
2024-01-04 21:59:42 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListComponent as CustomerListComponent } from './customers/list/list.component';
+import { ListComponent as UserListComponent } from './users/list/list.component';
+import { ListComponent as ProductListComponent } from './products/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+     { path: '', component: AppComponent },
+    { path: 'customers', component: CustomerListComponent },
+    { path: 'users', component: UserListComponent },
+    { path: 'products', component: ProductListComponent },
+
+];
+
+
---
+++ src/app/products/create-update/create-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-update.component.html',
+  styleUrl: './create-update.component.css'
+})
+export class CreateUpdateComponent {
+
+}
